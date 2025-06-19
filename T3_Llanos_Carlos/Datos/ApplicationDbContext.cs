0c32b9b3b94099f2b775f06d87dbaefc19f190f7
upn_i <upn_i@DESKTOP-1KG2GS2>
2025-06-19 03:19:47 +0200

    Agregando modelo a la Base de Datos

---
+++ T3_Llanos_Carlos/Datos/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using T3_Llanos_Carlos.Models;
+
+namespace UsandoCRUDValidaciones.Datos
+{
+    public class ApplicationDbContext:DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Libro>Libro { get; set; }
+
+    }
+}
