84260a701bde8583e8300dfff0f6fafb1dab8678
upn_i <upn_i@DESKTOP-1KG2GS2>
2025-06-19 03:38:00 +0200

    Agregando Index de Libro

---
+++ T3_Llanos_Carlos/Controllers/LibroController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using T3_Llanos_Carlos.Models;
+using UsandoCRUDValidaciones.Datos;
+
+namespace T3_Llanos_Carlos.Controllers
+{
+    public class LibroController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LibroController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Libro> lista = _db.Libro;
+            return View(lista);
+        }
+    }
+}
